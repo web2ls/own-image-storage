ef390a61a1fcf53325f333750958567074b96fe2
web2ls <web2ls@yandex.ru>
2020-12-17 18:11:26 +0300

    implement image upload handler on server side

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const fileUpload = require('express-fileupload');
+const morgan = require('morgan');
+const uploadHandler = require('./upload');
 
-app.use(cors())
+const PORT = process.env.PORT || 8000;
+
+app.use(fileUpload({
+  createParentPath: true
+}));
+app.use(morgan('dev'));
+app.use(cors());
 app.use(express.static('public'));
 
 app.get('/home', (req, res) => res.send('Hello user'));
+app.post('/upload', uploadHandler)
 
-app.listen(8000, () => {
+app.listen(PORT, () => {
   console.log('server has been started...')
 })
---
+++ backend/upload.js
@@ -0,0 +1,46 @@
+function uploadHandler(req, res) {
+  try {
+    if (!req.files) {
+      res.send({
+        status: 400,
+        message: 'No files for upload'
+      });
+      return;
+    };
+
+    const data = [];
+
+    if (!Array.isArray(req.files.images)) {
+      req.files.images.mv('./upload/' + req.files.images.name);
+      res.send({
+        status: 200,
+        message: 'Image has been uploaded',
+        data: {
+          name: req.files.images.name,
+          mimetype: req.files.images.mimetype,
+          size: req.files.images.size
+        }
+      });
+      return;
+    }
+
+    req.files.images.forEach(image => {
+      image.mv('./uploads/' + image.name);
+      data.push({
+        name: image.name,
+        mimetype: image.mimetype,
+        size: image.size
+      });
+    });
+
+    res.send({
+      status: 200,
+      message: 'Images has been uploaded',
+      data: data
+    })
+  } catch (error) {
+    res.status(500).send('system error');
+  }
+};
+
+module.exports = uploadHandler;
