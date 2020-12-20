0d98b94401c12fa3205e503545c944ee56e499bc
web2ls <web2ls@yandex.ru>
2020-12-20 19:30:18 +0300

    implement copy url to the image feature

--- frontend/src/components/image/index.js
+++ frontend/src/components/image/index.js
@@ -3,9 +3,25 @@ import style from './style.css';
 const Image = (props) => {
   const source = `http://localhost:8000/${props.image}`;
 
+  const copyImageUrl = () => {
+    const $textArea = document.createElement('textarea');
+    const $content = document.querySelector('.content');
+    $textArea.value = source;
+    $content.appendChild($textArea);
+    $textArea.focus();
+    $textArea.select();
+
+    try {
+      document.execCommand('copy');
+    } catch (err) {
+      console.error(err);
+    }
+    $content.removeChild($textArea);
+  };
+
   return (
     <div class={style.image}>
-      <div class={style.imagePreview}>
+      <div class={style.imagePreview} onClick={copyImageUrl}>
         <img src={source} class={style.imageItem} />
       </div>
       <div class={style.imageControls}>
