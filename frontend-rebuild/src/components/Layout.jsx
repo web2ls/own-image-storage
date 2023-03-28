439175959551f8d61143f6e154dbcac07e3ef9b1
web2ls <web2ls@yandex.ru>
2023-03-28 14:27:12 +0400

    Fix: eslint config problems

--- frontend-rebuild/src/components/Layout.jsx
+++ frontend-rebuild/src/components/Layout.jsx
@@ -1,5 +1,31 @@
+import styled from 'styled-components';
+
 function Layout() {
-  return <div>Layout</div>;
+  return (
+    <LayoutWrapper>
+      <div className="sidebar">Sidebar</div>
+      <div>
+        <header>Header</header>
+        <main>Main section content</main>
+      </div>
+    </LayoutWrapper>
+  )
 }
 
 export default Layout;
+
+const LayoutWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 1280px;
+  height: 100vh;
+  margin: 0 auto;
+
+  & .sidebar {
+    flex-basis: 300px;
+  }
+
+  & main {
+    flex: 1;
+  }
+`;
